b8df8f205e4ed6ba4948d1bded12e910d55e42c5
Julian Rudzki <julian95844@hotmail.com>
2023-08-21 20:22:52 -0300

    Agregando repositorio GitHub

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({children}) => {
+  return (
+    <div className="bg-red-800 uppercase text-center text-white p-3 font-bold mb-3 rounded-md">
+        {children}
+    </div>
+  )
+}
+
+export default Error
